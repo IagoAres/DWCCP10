cdd54ac8c72891961f194f5839e5e57d1e225966
IagoAres2 <iago.obradoiro.informatica@gmail.com>
2022-11-26 19:15:19 +0100

    Second full on

--- admin.js
+++ admin.js
@@ -1,56 +1,61 @@
-'use STRICT'
-var i = 0
+"use STRICT";
+var i = 0;
 var variables = [];
 var coordenadas = [];
 var tabla = document.getElementById("tabla");
+var th_span = document.getElementById("th");
+
 window.onload = function () {
-    tabla.addEventListener("click", function (e) {
-        switch (e.target.tagName) {
+    document.querySelectorAll(".texto").forEach((e) => {
 
-            case "SPAN":
-                if (e.target.innerHTML == coordenadas[i]) {
-                    console.log("true");
-                    console.log("-------------")
-                    i++;
-                } else {
-                    i = 0;
-                }
-                break;
-            
-            case "TH":
-                console.log(th);
-                break;
-            default:
+        e.addEventListener("click", function () {
 
-        }
+            if (e.innerHTML == coordenadas[i]) {
+
+                var div = document.createElement("div");
+                var p = document.createElement("p");
+                p.appendChild(document.createTextNode(e.innerHTML));
+                div.appendChild(p);
+                console.log(div);
+
+                th_span.insertAdjacentElement('beforeend',div);
+
+                console.log("true");
+                console.log("-------------");
+                i++;
+            } else {
+                i = 0;
+            }
+
+        })
+    });
 
 
-    })
 
-    document.querySelectorAll("table td span").forEach(element => {
-        if (variables.length <= 15) {
-            var valor = Math.round(Math.random() * (16 - 1) + 1);
-            
+    document.querySelectorAll(".texto").forEach((element) => {
+        if (variables.length <= 19) {
+            var valor = Math.round(Math.random() * (20 - 1) + 1);
+
             while (variables.includes(valor)) {
-                valor = Math.round(Math.random() * (16 - 1) + 1);
+                valor = Math.round(Math.random() * (20 - 1) + 1);
             }
             variables.push(valor);
             element.innerHTML = valor;
         }
 
-        var coor = Math.round(Math.random() * (16 - 1) + 1);
+        var coor = Math.round(Math.random() * (20 - 1) + 1);
 
         if (coordenadas.length <= 7) {
             while (coordenadas.includes(coor)) {
-                coor = Math.round(Math.random() * (16 - 1) + 1);
+                coor = Math.round(Math.random() * (20 - 1) + 1);
             }
             coordenadas.push(coor);
-
         }
-    })
+    });
 
     console.log("------------------------------");
-    coordenadas.forEach(element => {
+    coordenadas.forEach((element) => {
         console.log(element);
     });
-}
+
+};
